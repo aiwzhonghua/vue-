2d9fe7f880bbc67c1024262aa6621a6b542b829f
aiwzhonghua <962738330@qq.com>
2025-09-24 21:33:42 +0800

    feat: 添加用户登录功能

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
-/* eslint-env node */
 module.exports = {
-  printWidth: 120,
-  tabWidth: 4,
-  useTabs: true,
-  semi: true,
-  singleQuote: false,
-  endOfLine: "auto",
-  trailingComma: "none",
+    printWidth: 120, //单行长度
+    tabWidth: 4, //缩进长度
+    useTabs: false, //使用空格代替tab缩进
+    semi: true, //句末使用分号
+    singleQuote: true, //使用单引号
+    endOfLine: 'auto',
+    // indent: 4, //缩进长度
+    trailingComma: 'none' // 对象最后一个属性末尾是否要逗号
 };
